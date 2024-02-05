cea7fe74e5654bbdc4c46cbbe4e0e7e3d93961b6
Lohann Kasper <lohann.kasper@me.com>
2024-02-05 12:54:24 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+// app.js
+import { generatePalette, isHexColor } from "./modules/utils";
+
+// Cherche l'élément <form> dans le DOM
+const formElement = document.querySelector("form");
+
+const handleForm = (e) => {
+    try {
+        // Empêche le refresh lors de la soumission du formulaire
+        e.preventDefault();
+        // Cherche la valeur de l'élément <input>
+        const inputValue = document.querySelector('input').value;
+        // Vérifie que la valeur soit bien un code hexadécimal
+        if (!isHexColor(inputValue)) {
+            // Si ce n'est pas le cas, balancer l'erreur
+            throw new Error(`${inputValue} is not a valid Hexadecimal color`);
+        }
+
+        // Crée la palette à partir du code hexadécimal
+        const palette = generatePalette(inputValue);
+        // Affiche dans la console la valeur d'entrée et la palette
+        console.log(inputValue, palette);
+    } catch (err) {
+        // Attrape les erreurs du block try et les affiche dans la console.
+        console.error(err);
+    }
+};
+
+// Lorsque le formulaire est soumis, appèle le callback handleForm
+formElement.addEventListener("submit", handleForm);
